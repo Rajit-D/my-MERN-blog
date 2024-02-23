58ccd56f6bbfd81aaa11a973acededf1ad377157
Rajit-D <rajitdutta2002@gmail.com>
2024-02-23 21:24:59 +0530

    sidebar updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,24 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar.jsx";
+import DashProfile from "../components/DashProfile.jsx";
 
-export default Dashboard
+const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParam = new URLSearchParams(location.search);
+    const tabURL = urlParam.get("tab");
+    if (tabURL) setTab(tabURL);
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="">
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
+};
+
+export default Dashboard;
